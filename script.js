0b15352b87483623c78ecbbb7fa3eb61b035d231
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-15 19:54:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+/* 
+
+Pobierz do zmiennej jsonPracownicy poniższy obiekt:
+
+{
+    "pracownicy":[
+        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+        {"firstName": "Anna", "lastName": "Szapiel"},
+        {"firstName": "Piotr", "lastName": "Żmuda"}
+    ]
+}
+
+Wyświetl go w konsoli, a następnie za pomocą pętli forEach przeiteruj po każdym pracowniku, oraz wyświetl w konsoli index elementu tablicy oraz imie i nazwisko pracownika.
+
+Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+ */
