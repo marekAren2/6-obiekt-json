b91e3404682b2dd05885eeb8c24df2d2ac3d61aa
marekAren2 <marekaren83@gmail.com>
2023-10-16 12:58:07 +0200

    6-json vsc last

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-
 const jsonPracownicy = {
-  "pracownicy": [
-    { "firstName": "Krystian", "lastName": "Dziopa" },
-    { "firstName": "Anna", "lastName": "Szapiel" },
-    { "firstName": "Piotr", "lastName": "Żmuda" }
-  ]
+    "pracownicy":[
+        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+        {"firstName": "Anna", "lastName": "Szapiel"},
+        {"firstName": "Piotr", "lastName": "Żmuda"}
+    ]
 }
-// console.log(pracownicy) nie zdfiniowany
-console.log(jsonPracownicy;
 
-jsonPracownicy.forEarch(function(pracownik) {
-  // console.log(pracownik.firstName + ' ' + pracownik.lastName)
-  console.log(pracownik);
-})
+console.log(jsonPracownicy); // nie zdefiniowany
 
+console.log(jsonPracownicy.pracownicy);
+
+// jsonPracownicy.pracownicy.forEarch(function(pracownik) {
+// // console.log(pracownik.firstName + ' ' + pracownik.lastName)
+// console.log(pracownik);
+//  })
+
+jsonPracownicy.pracownicy.forEach((record) => console.log(record));
+jsonPracownicy.pracownicy.forEach(
+    (record,index) => 
+    console.log(index,record.firstName,record.lastName));
 
 /*
 Pobierz do zmiennej jsonPracownicy poniższy obiekt:
@@ -26,7 +31,9 @@ Pobierz do zmiennej jsonPracownicy poniższy obiekt:
     ]
 }
 
-Wyświetl go w konsoli, a następnie za pomocą pętli forEach przeiteruj po każdym pracowniku, oraz wyświetl w konsoli index elementu tablicy oraz imie i nazwisko pracownika.
+Wyświetl go w konsoli, a następnie za pomocą pętli forEach przeiteruj
+ po każdym pracowniku, oraz wyświetl 
+ w konsoli index elementu tablicy oraz imie i nazwisko pracownika.
 
 Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
  */
